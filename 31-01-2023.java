101fc4812b4973622537b610f898e40a1aa4ccf1
lovishgupta1001 <lovishgupta1001@gmail.com>
2023-01-31 23:53:46 +0530

    31-01-2023 gfg potd

---
+++ 31-01-2023.java
@@ -0,0 +1,30 @@
+
+
+//User function Template for Java
+
+class Solution {
+    static int minRepeats(String a, String b) {
+       
+        
+ String rep = a;
+ 
+ int c = 1; 
+ 
+ while(a.length()<b.length()){
+     a+=rep;
+     c++;
+ }
+     
+   
+     
+     if(a.contains(b)==true)return c;
+     a+=rep;
+     c++;
+    
+       if(a.contains(b)==true)return c;
+       return -1;
+ 
+        
+   
+    }
+};
