581cf95f9b39aae7e9088481d5a6d12510928d08
lovishgupta1001 <lovishgupta1001@gmail.com>
2023-02-04 23:56:49 +0530

    04-02-2023 gfg potd

--- 04-02-2023.cpp
+++ 04-02-2023.cpp
@@ -20,3 +20,29 @@ public:
         return prev;
     }
 };
+
+// Also DP Approach is here with the help of Memoization in which we use recusrion and do the process recursively\
+//User function template for C++
+
+class Solution
+{
+public:
+    // calculate the maximum sum with out adjacent
+    int fun(int i, int *arr, int n, vector<int> &dp)
+    {
+        if (i >= n)
+            return 0;
+        if (dp[i] != -1)
+            return dp[i];
+        int tk = 0, nt = 0;
+        nt = fun(i + 1, arr, n, dp);
+        tk = arr[i] + fun(i + 2, arr, n, dp);
+        return dp[i] = max(tk, nt);
+    }
+    int findMaxSum(int *arr, int n)
+    {
+        // code here
+        vector<int> dp(n + 1, -1);
+        return fun(0, arr, n, dp);
+    }
+};
